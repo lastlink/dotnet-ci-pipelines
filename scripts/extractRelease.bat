b5227ea15b464e9ca74cab28511f2fdd85a070ad
lastlink <theefunk@gmail.com>
2020-10-16 09:44:40 -0400

    init working files

---
+++ scripts/extractRelease.bat
@@ -0,0 +1,11 @@
+@REM copy dlls wanted to artifact folder
+set folder=Release\pre
+folder=Release\pre
+echo %folder%
+mkdir %folder%'
+
+.\tools\obfuscar.console .\scripts\obfuscator\win\obfuscar-Api.xml .\scripts\obfuscator\win\obfuscar-console.xml .\scripts\obfuscator\win\obfuscar-Repository.xml
+
+copy "MyProject\bin\Release\netcoreapp2.2\publish\ReadyForDeployment\MyProject.dll" "%folder%\MyProject.dll" /y
+copy "MyProject.Api\bin\Release\netcoreapp2.2\publish\ReadyForDeployment\MyProject.Api.dll" "%folder%\MyProject.Api.dll" /y
+copy "MyProject.Repository\bin\Release\netcoreapp2.2\publish\ReadyForDeployment\MyProject.Repository.dll" "%folder%\MyProject.Repository.dll" /y
