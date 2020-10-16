133108021b41c9a4079bf7c077a56d11e25051fa
lastlink <theefunk@gmail.com>
2020-10-16 10:14:58 -0400

    dotnet core 3

--- scripts/extractRelease.bat
+++ scripts/extractRelease.bat
@@ -6,6 +6,6 @@ mkdir %folder%'
 
 .\tools\obfuscar.console .\scripts\obfuscator\win\obfuscar-Api.xml .\scripts\obfuscator\win\obfuscar-console.xml .\scripts\obfuscator\win\obfuscar-Repository.xml
 
-copy "MyProject\bin\Release\netcoreapp2.2\publish\ReadyForDeployment\MyProject.dll" "%folder%\MyProject.dll" /y
-copy "MyProject.Api\bin\Release\netcoreapp2.2\publish\ReadyForDeployment\MyProject.Api.dll" "%folder%\MyProject.Api.dll" /y
-copy "MyProject.Repository\bin\Release\netcoreapp2.2\publish\ReadyForDeployment\MyProject.Repository.dll" "%folder%\MyProject.Repository.dll" /y
+copy "MyProject\bin\Release\netcoreapp3.1\publish\ReadyForDeployment\MyProject.dll" "%folder%\MyProject.dll" /y
+copy "MyProject.Api\bin\Release\netcoreapp3.1\publish\ReadyForDeployment\MyProject.Api.dll" "%folder%\MyProject.Api.dll" /y
+copy "MyProject.Repository\bin\Release\netcoreapp3.1\publish\ReadyForDeployment\MyProject.Repository.dll" "%folder%\MyProject.Repository.dll" /y
--- scripts/extractRelease.sh
+++ scripts/extractRelease.sh
@@ -7,5 +7,5 @@ mkdir -p ${folder}
 ./tools/obfuscar.console ./scripts/obfuscator/unix/obfuscar-Api.xml ./scripts/obfuscator/unix/obfuscar-console.xml ./scripts/obfuscator/unix/obfuscar-Repository.xml
 
 cp MyProject/bin/Release/netcoreapp2.0/publish/ReadyForDeployment/MyProject.dll ${folder}
-cp MyProject.Api/bin/Release/netcoreapp2.2/publish/ReadyForDeployment/MyProject.Api.dll ${folder}
-cp MyProject.Repository/bin/Release/netcoreapp2.2/publish/ReadyForDeployment/MyProject.Repository.dll ${folder}
+cp MyProject.Api/bin/Release/netcoreapp3.1/publish/ReadyForDeployment/MyProject.Api.dll ${folder}
+cp MyProject.Repository/bin/Release/netcoreapp3.1/publish/ReadyForDeployment/MyProject.Repository.dll ${folder}
