3afd2e7dfbe7577684a4a7c01716f5a6a41409c2
lastlink <theefunk@gmail.com>
2020-10-17 01:30:02 -0400

    more tweaks

--- scripts/extractRelease.sh
+++ scripts/extractRelease.sh
@@ -6,6 +6,6 @@ mkdir -p ${folder}
 
 ./tools/obfuscar.console ./scripts/obfuscator/unix/obfuscar-Api.xml ./scripts/obfuscator/unix/obfuscar-console.xml ./scripts/obfuscator/unix/obfuscar-Repository.xml
 
-cp MyProject/bin/Release/netcoreapp2.0/publish/ReadyForDeployment/MyProject.dll ${folder}
+cp MyProject/bin/Release/netcoreapp3.1/publish/ReadyForDeployment/MyProject.dll ${folder}
 cp MyProject.Api/bin/Release/netcoreapp3.1/publish/ReadyForDeployment/MyProject.Api.dll ${folder}
 cp MyProject.Repository/bin/Release/netcoreapp3.1/publish/ReadyForDeployment/MyProject.Repository.dll ${folder}
